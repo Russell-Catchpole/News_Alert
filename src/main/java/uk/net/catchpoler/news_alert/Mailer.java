d25a3a9904d1aa15d81a446e95cf76374a589b34
Russell-Catchpole <russelljcatchpole@gmail.com>
2022-10-20 17:40:55 +0100

    Added search term iteration 2

--- src/main/java/uk/net/catchpoler/news_alert/Mailer.java
+++ src/main/java/uk/net/catchpoler/news_alert/Mailer.java
@@ -9,7 +9,7 @@ public class Mailer {
         String to = "rcatchpo@yahoo.co.uk";
         String from = "russelljcatchpole@googlemail.com";
         final String username = "russelljcatchpole@googlemail.com";
-        final String password = "fnwpttgrtxggsdet";
+        final String password = "";
         String host = "smtp.gmail.com";
 
         // Get system properties
